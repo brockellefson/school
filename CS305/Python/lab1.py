8e6c4380a1b47e7e4695a3fc913d346f805fb20e
Brock Ellefson <brockilli03@gmail.com>
2017-03-12 12:08:01 -0600

    python lab complete

--- CS305/Python/lab1.py
+++ CS305/Python/lab1.py
@@ -7,13 +7,14 @@ from collections import Counter, defaultdict
 
 # Replace the string value of the following variable with your names.
 ME = 'Brock Ellefson';
-COLLABORATORS = ['Satan']
+COLLABORATORS = []
 
 def process_file(infile):
     titles = set()
 
     # Loop through each line of the file
     print ('Processing file')
+    
     for line in infile:
         line = line.rstrip()
 
@@ -32,6 +33,7 @@ def process_file(infile):
         #lower case and adds song title 'titles' list
         lower_case_clean_title = data.lower()
         titles.add(lower_case_clean_title)
+
     print('File has been processed\n')
 
 ##################################################################################
@@ -40,10 +42,8 @@ def process_file(infile):
 
     #this is the dictonary where all the bigrams are stored
     d = defaultdict(int)
-    bigram_count = 'ERROR: bigram empty'
-
-
     for title in titles:
+        #find all the words in a string
         bigram_count = re.findall("\w+",title)
 
         #counts all bigrams in a single line
@@ -52,13 +52,8 @@ def process_file(infile):
             #key is the bigram, value is the frequency
             d[key] += 1
 
-
-
     print ('Bi-Gram Contructed\n')
 ##################################################################################
-
-
-
     # using bigram_count, find most common word following 'word'
     def most_common_word(word):
         high = 0
@@ -77,27 +72,24 @@ def process_file(infile):
                     common_word = key[1]
         return common_word
 
-
     #bigrams song title generator for EC
     def create_song_title(word, n):
         create_title = []
         create_title.append(word)
         next_word = word
         for w in range(n):
+            #append a space followed by the next word to a list, make the list a string
             next_word = most_common_word(next_word)
             create_title.append(' ')
             create_title.append(next_word)
         song_title = ''.join(create_title)
         return song_title
 
-
-
-    # return most common word
+    #basic ui
     def test_bigram():
         exit = 0
-        input_string = 'null'
         while exit !=1:
-            input_string = input('\nPlease enter a word to see its most common follow up word\n(type "exit" to exit or generate_title to generate a song title): ')
+            input_string = input('\nPlease enter a word to see its most common follow up word\n(type "generate_title" to generate a title or "exit" to exit): ')
             if input_string == 'exit':
                 exit = 1
             elif input_string == 'generate_title':
@@ -111,20 +103,18 @@ def process_file(infile):
 
     return most_common_word
 
-
 ##################################################################################
-
 # DON'T WORRY ABOUT CODE BELOW HERE, IT JUST MAKES YOUR LIVE EASIER
 def get_file_name():
     parser = argparse.ArgumentParser()
-    parser.add_argument('subsampled_unique_tracks.txt', action='store_true')
+    parser.add_argument('subsampled_unique_tracks.txt')
     return parser.parse_args().file_name
 
 
 def main():
     print ('CSCI 305 Lab 1 submitted by %s' % ME)
     print ('  with help from %s\n\n' % ', '.join(COLLABORATORS))
-    #file_name = get_file_name()
+    #file_name = get_file_name() <-- this kept giving me an error so i opened the file my own way
     with open('subsampled_unique_tracks.txt', encoding="utf8") as infile:
         process_file(infile)
 
