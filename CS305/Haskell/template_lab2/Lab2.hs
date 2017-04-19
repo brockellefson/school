6b2bcc2e421eb9ac812d9add0304ab5c673942f1
Brock Ellefson <brockilli03@gmail.com>
2017-04-18 23:49:38 -0600

    305 Haskell Done

--- CS305/Haskell/template_lab2/Lab2.hs
+++ CS305/Haskell/template_lab2/Lab2.hs
@@ -1,6 +1,13 @@
 
 module Lab2
-(f) where
+(f
+, fForIntegral
+, member
+,validSet
+,setEQ
+,unionEQ
+,intersection
+) where
 import Data.List
 
 -------------------------------------------------------------------------------
@@ -16,38 +23,32 @@ f list k =
 -------------------------------------------------------------------------------
     -- Pattern Matching
 fForIntegral:: [Int] -> Int -> [Int] --take in an list of ints, and an int, return a list of ints
-fForIntegral l a = [x + a| x <- l] --take the list(l) and the int(a), add the int to each element (x) in the list
+fForIntegral as a = [b + a | b <- as] --take the list(as) and the int(a), add the int to each element (b) in the list
 -------------------------------------------------------------------------------
     -- Set membership
-member:: Eq a => a -> [a] -> Bool --for anything in the EQ typeclass, take an an a, a list of the same typeclass, and return a bool
+member:: (Eq a) => a -> [a] -> Bool -- take in some type a, some list a, and return a bool
 member a [] = False --if the list is empty return false
-member a (b:l)
- | a == b = True --a and the head of the list are equalvilent
- | otherwise = member a (l) --repeat with augmented list
+member a (b:bs) -- element a and head of list bs
+ | a == b = True --a and the head of the list are equalvilent, then it is in the list
+ | otherwise = member a (bs) --iterate if not found
 
--------------------------------------------------------------------------------
+-----------------------------------------------------------------------------
     -- Valid set
---validSet:: Eq a => [a] -> Bool
---validSet a
---validSet b = head a
---validSet c = last a
---validSet l = [b..c]
---validSet (a:as) (l:ls)
---  | a == l = True
---  | otherwise = False
-
+validSet:: (Eq a) => [a] -> Bool --Takes in a list, returns bool
+validSet [] = True --empty set
+validSet [a] = True
+validSet (a:b:as) = if a == b then False else validSet (b:as) --if head is equal to head+1 the set is not valid. traverse until empty
 
 -------------------------------------------------------------------------------
     -- Set eq
-setEQ:: [Int] -> [Int] -> Bool --takes in 2 lists
-setEQ a b = sort a == sort b --sorts them, them
-
-setEQ1::Eq a => [a] -> [a] -> Bool
-setEQ1  a b = sort a == sort b --sorts them, them
-
+setEQ:: (Eq a) => [a] -> [a] -> Bool --take an a list a, another list a, and returns a bool
+setEQ  as bs = [a | a <- as, member a bs] == as --take the intersection of a and b, if this new list is equal to as, then the two lists are the same
 -------------------------------------------------------------------------------
     -- Union
-
+unionEQ:: (Eq a) => [a] -> [a] -> [a] --take in 2 lists and returns a list
+unionEQ as bs = as ++ bs --adds both lists together
 
 -------------------------------------------------------------------------------
     -- Intersecton
+intersection:: (Eq a) => [a] -> [a] -> [a]
+intersection as bs = [a | a <- as, member a bs] --list cs equals all elements of a that are in bs
