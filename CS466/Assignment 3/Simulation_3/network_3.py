45bd76c579c552900ae4a99f0b0e7718d49272d2
Brock Ellefson <brockilli03@gmail.com>
2017-11-01 23:30:32 -0600

    part 3 progress

---
+++ CS466/Assignment 3/Simulation_3/network_3.py
@@ -0,0 +1,207 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import queue
+import threading
+
+
+## wrapper class for a queue of packets
+class Interface:
+    ## @param maxsize - the maximum size of the queue storing packets
+    def __init__(self, maxsize=0):
+        self.queue = queue.Queue(maxsize);
+        self.mtu = None
+
+    ##get packet from the queue interface
+    def get(self):
+        try:
+            return self.queue.get(False)
+        except queue.Empty:
+            return None
+
+    ##put the packet into the interface queue
+    # @param pkt - Packet to be inserted into the queue
+    # @param block - if True, block until room in queue, if False may throw queue.Full exception
+    def put(self, pkt, block=False):
+        self.queue.put(pkt, block)
+
+## Implements a network layer packet (different from the RDT packet
+# from programming assignment 2).
+# NOTE: This class will need to be extended to for the packet to include
+# the fields necessary for the completion of this assignment.
+class NetworkPacket:
+    ## packet encoding lengths
+
+
+    #PACKET HEADER
+    #dst_addr:frag_flag:packetid:msg
+
+
+    dst_addr_S_length = 5
+    frag_flag_S_length = 1
+    ident_S_length = 5
+    ##@param dst_addr: address of the destination host
+    # @param data_S: packet payload
+    # @param frag_flag: if the packet is part of a segmentation
+    # @param offset: where the offset is
+    def __init__(self, dst_addr, frag_flag, ident, data_S):
+        self.dst_addr = dst_addr
+        self.data_S = data_S
+        self.frag_flag = frag_flag
+        self.ident = ident
+    ## called when printing the object
+    def __str__(self):
+        return self.to_byte_S()
+
+    ## convert packet to a byte string for transmission over links
+    def to_byte_S(self):
+        byte_S = str(self.dst_addr).zfill(self.dst_addr_S_length)
+        byte_S += str(self.frag_flag).zfill(self.frag_flag_S_length)
+        byte_S += str(self.ident).zfill(self.ident_S_length)
+        byte_S += self.data_S
+        return byte_S
+
+    ## extract a packet object from a byte string
+    # @param byte_S: byte string representation of the packet
+    @classmethod
+    def from_byte_S(self, byte_S):
+        dst_addr = int(byte_S[0 : NetworkPacket.dst_addr_S_length])
+        frag_flag = int(byte_S[NetworkPacket.dst_addr_S_length : NetworkPacket.dst_addr_S_length + NetworkPacket.frag_flag_S_length])
+        ident = int(byte_S[NetworkPacket.dst_addr_S_length + NetworkPacket.frag_flag_S_length : NetworkPacket.dst_addr_S_length + NetworkPacket.frag_flag_S_length + NetworkPacket.ident_S_length ])
+        data_S = byte_S[NetworkPacket.dst_addr_S_length + NetworkPacket.frag_flag_S_length + NetworkPacket.ident_S_length:]
+        return self(dst_addr, frag_flag, ident, data_S)
+
+
+    def segment(self, mtu):
+        segments = [self.data_S[i:i+mtu] for i in range(0,len(self.data_S), mtu)] #break packet into segments of mtu length
+        return segments
+
+## Implements a network host for receiving and transmitting data
+class Host:
+    segment_list = []
+    ##@param addr: address of this node represented as an integer
+    def __init__(self, addr):
+        self.addr = addr
+        self.in_intf_L = [Interface()]
+        self.out_intf_L = [Interface()]
+        self.stop = False #for thread termination
+
+    ## called when printing the object
+    def __str__(self):
+        return 'Host_%s' % (self.addr)
+
+    ## create a packet and enqueue for transmission
+    # @param dst_addr: destination address for the packet
+    # @param data_S: data being transmitted to the network layer
+    def multiPackets(self, data_S, mtu):
+        packets = [data_S[i:i+mtu] for i in range(0,len(data_S), mtu)]
+        return packets
+
+
+    def udt_send(self, dst_addr, data_S):
+        if (len(data_S) > self.out_intf_L[0].mtu): #string bigger than mtu, send multiple packets
+            string_mtu =  self.out_intf_L[0].mtu - 11 #7 is the bit addr length
+            packets = self.multiPackets(data_S, string_mtu)
+            p_id = 10000 #packet id
+            for packet in packets:
+                p = NetworkPacket(dst_addr, 0, p_id, packet)
+                self.out_intf_L[0].put(p.to_byte_S())
+                print('\n%s: sending packet "%s" out interface with mtu=%d\n' % (self, p, self.out_intf_L[0].mtu))
+                p_id+= 10000
+        else:
+            p = NetworkPacket(dst_addr, 0, p_id, data_S)
+            self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
+            print('\n%s: sending packet "%s" out interface with mtu=%d\n' % (self, p, self.out_intf_L[0].mtu))
+
+    def reconstruct(self, segments, pkt_id):
+        og_pkt = ''
+        for segment in segments:
+            if segment[6] == pkt_id: #for all segments that share the same id
+                og_pkt += segment[11:] #add all segments into return string
+        return og_pkt
+
+    ## receive packet from the network layer
+    def udt_receive(self):
+        pkt_S = self.in_intf_L[0].get()
+        if pkt_S is not None:
+            print('\n%s: received packet "%s"\n' % (self, pkt_S))
+            if pkt_S[5] == '1': #if a packet is actually a segment
+                self.segment_list.append(pkt_S) #add to list of all segmented packets
+                if pkt_S[7] == pkt_S[9] and pkt_S[8] == pkt_S[10]: #if the total segments equals the latest packet part of the id
+                    og_data = self.reconstruct(self.segment_list, pkt_S[6]) #reconstruct original string
+                    p = NetworkPacket(pkt_S[:5], 0, pkt_S[6] + '0000', og_data) #reconstruct original packet
+                    print('\nCONSTRUCTED PACKET: ' + p.to_byte_S() + '\n')
+
+
+
+    ## thread target for the host to keep receiving data
+    def run(self):
+        print (threading.currentThread().getName() + ': Starting ')
+        while True:
+            #receive data arriving to the in interface
+            self.udt_receive()
+            #terminate
+            if(self.stop):
+                print (threading.currentThread().getName() + ': Ending ')
+                return
+
+
+
+## Implements a multi-interface router described in class
+class Router:
+
+    ##@param name: friendly router name for debugging
+    # @param intf_count: the number of input and output interfaces
+    # @param max_queue_size: max queue length (passed to Interface)
+    def __init__(self, name, intf_count, max_queue_size):
+        self.stop = False #for thread termination
+        self.name = name
+        #create a list of interfaces
+        self.in_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
+        self.out_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
+
+    ## called when printing the object
+    def __str__(self):
+        return 'Router_%s' % (self.name)
+
+    ## look through the content of incoming interfaces and forward to
+    # appropriate outgoing interfaces
+    def forward(self):
+        for i in range(len(self.in_intf_L)):
+            pkt_S = None
+            try:
+                #get packet from interface i
+                pkt_S = self.in_intf_L[i].get()
+                #if packet exists make a forwarding decision
+                if pkt_S is not None:
+                    p = NetworkPacket.from_byte_S(pkt_S) #parse a packet out
+                    if (len(pkt_S) > self.out_intf_L[i].mtu):
+                        string_mtu =  self.out_intf_L[i].mtu - 11 #11 is the bit addr length
+                        segments = p.segment(string_mtu) #segment packet into segments
+                        seg_total = len(segments) * 100 #total amount of segments
+                        seg_count = 1
+                        for seg in segments:
+                            seg_p = NetworkPacket(p.dst_addr, 1, p.ident + seg_total + seg_count, seg) #segment packet
+                            self.out_intf_L[i].put(seg_p.to_byte_S(), True) #send seg_p
+                            print('\n%s: forwarding segmented packet "%s" from interface %d to %d with mtu %d\n' \
+                                % (self, seg_p, i, i, self.out_intf_L[i].mtu))
+                            seg_count += 1
+
+                    else:
+                        self.out_intf_L[i].put(p.to_byte_S(), True)
+                        print('\n%s: forwarding packet "%s" from interface %d to %d with mtu %d\n' \
+                            % (self, p, i, i, self.out_intf_L[i].mtu))
+            except queue.Full:
+                print('\n%s: packet "%s" lost on interface %d\n' % (self, p, i))
+                pass
+
+    ## thread target for the host to keep forwarding data
+    def run(self):
+        print (threading.currentThread().getName() + ': Starting ')
+        while True:
+            self.forward()
+            if self.stop:
+                print (threading.currentThread().getName() + ': Ending ')
+                return
---
+++ CS466/Assignment 3/Simulation_3/simulation_3.py
@@ -0,0 +1,101 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import network_3
+import link_3
+import threading
+from time import sleep
+
+##configuration parameters
+router_queue_size = 0 #0 means unlimited
+simulation_time = 1 #give the network sufficient time to transfer all packets before quitting
+
+
+
+
+if __name__ == '__main__':
+    object_L = [] #keeps track of objects, so we can kill their threads
+
+    #create network nodes
+    host_1 = network_3.Host(1) #Host 1
+    object_L.append(host_1)
+
+    host_2 = network_3.Host(2) #Host 2
+    object_L.append(host_2)
+
+    host_3 = network_3.Host(3) #Host 3
+    object_L.append(host_3)
+
+    host_4 = network_3.Host(4) #Host 4
+    object_L.append(host_4)
+
+    router_a = network_3.Router(name='A', intf_count=1, max_queue_size=router_queue_size) #Router A
+    object_L.append(router_a)
+
+    router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size) #Router B
+    object_L.append(router_b)
+
+    router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size) #Router C
+    object_L.append(router_c)
+
+    router_d = network_3.Router(name='D', intf_count=1, max_queue_size=router_queue_size) #Router D
+    object_L.append(router_d)
+
+
+    #create a Link Layer to keep track of links between network nodes
+    link_layer = link_3.LinkLayer()
+    object_L.append(link_layer)
+
+    #add all the links
+    link_layer.add_link(link_3.Link(host_1, 0, router_a, 0, 50)) #Host 1 -> Router A
+    link_layer.add_link(link_3.Link(host_2, 0, router_a, 1, 50)) #Host 2 -> Router A
+
+    link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 30)) #Router A -> Router B
+    link_layer.add_link(link_3.Link(router_a, 1, router_c, 1, 30)) #Router A -> Router C
+
+    link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 30)) #Router B -> Router D
+
+    link_layer.add_link(link_3.Link(router_c, 0, router_d, 1, 30)) #Router C -> Router D
+
+    link_layer.add_link(link_3.Link(router_d, 0, host_3, 0, 30)) #Router D -> Host 3
+    link_layer.add_link(link_3.Link(router_d, 1, host_4, 0, 30)) #Router D -> Host 4
+
+    #start all the objects
+    thread_L = []
+    thread_L.append(threading.Thread(name=host_1.__str__(), target=host_1.run))
+    thread_L.append(threading.Thread(name=host_2.__str__(), target=host_2.run))
+    thread_L.append(threading.Thread(name=host_3.__str__(), target=host_3.run))
+    thread_L.append(threading.Thread(name=host_4.__str__(), target=host_4.run))
+
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
+
+    thread_L.append(threading.Thread(name="Network", target=link_layer.run))
+
+    for t in thread_L:
+        t.start()
+
+
+    #create some send events
+    kanye_data = 'The plan was to drink until the pain over but whats worse the pain or the hangover' #yeezy yeezy keep it wavy
+    host_1.udt_send(2, kanye_data) #send a fire bar
+
+
+    #give the network sufficient time to transfer all packets before quitting
+    sleep(simulation_time)
+
+    #join all threads
+    for o in object_L:
+        o.stop = True
+    for t in thread_L:
+        t.join()
+
+    print("All simulation threads joined")
+
+
+
+# writes to host periodically
