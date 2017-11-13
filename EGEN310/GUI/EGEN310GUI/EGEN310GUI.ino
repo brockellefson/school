4447c362ece4db327b97c72499f92d6a9807e326
brockellefson <brockilli03@gmail.com>
2017-11-12 22:30:40 -0700

    310

--- EGEN310/GUI/EGEN310GUI/EGEN310GUI.ino
+++ EGEN310/GUI/EGEN310GUI/EGEN310GUI.ino
@@ -29,59 +29,21 @@ Adafruit_DCMotor *LMotor = AFMS.getMotor(4);
 int state = 0;
 
 
-// A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
-}
-
-// function prototypes over in packetparser.cpp
-uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
-float parsefloat(uint8_t *buffer);
-void printHex(const uint8_t * data, const uint32_t numBytes);
-
-// the packet buffer
-extern uint8_t packetbuffer[];
-
-
 void setup(void)
 {
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
 
   Serial.begin(115200);
   Serial.println(F("Jaquel is online"));
   Serial.println(F("-----------------------------------------"));
-
-  /* Initialise the module */
   Serial.print(F("Initialising the Bluefruit LE module: "));
-
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
-  Serial.println( F("OK!") );
-
-  if ( FACTORYRESET_ENABLE )
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
-      error(F("Couldn't factory reset"));
-    }
-  }
-
-
-  /* Disable command echo from Bluefruit */
+  ble.begin();
   ble.echo(false);
-
   Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
   ble.info();
   
    //Start Motors
   AFMS.begin();  // create with the default frequency 1.6KHz
-  Serial.println(F("******************************"));
+  Serial.println(F("----------------------------------------"));
   LMotor->setSpeed(150);
   LMotor->run(FORWARD);
   LMotor->run(RELEASE);
@@ -91,37 +53,31 @@ void setup(void)
   RMotor->run(FORWARD);
   RMotor->run(RELEASE);
   Serial.println(F("Motor 2 Running"));
-  Serial.println(F("******************************"));
-  
-  ble.verbose(false);  // debug info is a little annoying after this point!
+  Serial.println(F("----------------------------------------"));
 
   /* Wait for connection */
   while (! ble.isConnected()) {
       delay(500);
   }
+  
+  Serial.println(F("Connected."));
 
-  Serial.println(F("******************************"));
-
-  // LED Activity command is only supported from 0.6.6
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
-  {
-    // Change Mode LED Activity
-    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
-    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
-  }
+  // Change Mode LED Activity
+  ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
 
   // Set Bluefruit to DATA mode
   Serial.println( F("Ready to recieve data") );
   ble.setMode(BLUEFRUIT_MODE_DATA);
 
-  Serial.println(F("******************************"));
+  Serial.println(F("----------------------------------------"));
 
 }
 
 void loop(void)
 {
-  if(ble.available() > 0){ // Checks whether data is comming from the serial port
-    state = ble.read(); // Reads the data from the serial port
+ if(ble.available() > 0){ // Checks whether data is comming from the serial port
+  state = ble.read(); // Reads the data from the serial port
+  Serial.println(state);
  }
  if (state == '0') {
   RMotor->run(RELEASE);
@@ -153,5 +109,9 @@ void loop(void)
   LMotor->run(BACKWARD);
   Serial.println((char)state);  
   Serial.println("Jaquel is going backwards");
+  }
+  else{
+  RMotor->run(RELEASE);
+  LMotor->run(RELEASE);  
   }
 }
