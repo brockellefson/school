fee91e8041b46fb7339974b796c13a6bc713dc98
brockellefson <brockilli03@gmail.com>
2017-11-25 20:34:27 -0700

    update

--- EGEN310/GUI/Open Source/EGEN310GUI/EGEN310GUI.ino
+++ EGEN310/GUI/Open Source/EGEN310GUI/EGEN310GUI.ino
@@ -10,17 +10,6 @@
 
 #include "BluefruitConfig.h"
 
-#if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
-#endif
-
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
-
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //create shield
 //Create Motors
 Adafruit_DCMotor *RMotor = AFMS.getMotor(3);
@@ -32,14 +21,11 @@ int state = 0;
 void setup(void)
 {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println(F("Jaquel is online"));
   Serial.println(F("-----------------------------------------"));
-  Serial.print(F("Initialising the Bluefruit LE module: "));
-  ble.begin();
-  ble.echo(false);
   Serial.println("Requesting Bluefruit info:");
-  ble.info();
+
   
    //Start Motors
   AFMS.begin();  // create with the default frequency 1.6KHz
@@ -55,49 +41,40 @@ void setup(void)
   Serial.println(F("Motor 2 Running"));
   Serial.println(F("----------------------------------------"));
 
-  // Set Bluefruit to DATA mode
-  Serial.println( F("Ready to recieve data") );
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-
   Serial.println(F("----------------------------------------"));
 
 }
 
 void loop(void)
 {
- if(ble.available() > 0){ // Checks whether data is comming from the serial port
-  state = ble.read(); // Reads the data from the serial port
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+  state = Serial.read(); // Reads the data from the serial port
   Serial.println(state);
  }
  if (state == '0') {
   RMotor->run(RELEASE);
   LMotor->run(RELEASE);
-  Serial.println((char)state);
   Serial.println("Jaquel has stopped"); // Send back, to the phone
   state = 0;
  }
  else if (state == '1') {
   RMotor->run(FORWARD);
   LMotor->run(FORWARD);
-  Serial.println((char)state);
   Serial.println("Jaquel is running forward");
  } 
  else if (state == '2') {
   RMotor->run(FORWARD);
   LMotor->run(BACKWARD);
-  Serial.println((char)state);  
   Serial.println("Jaquel is turning left");
  }
   else if (state == '3') {
   LMotor->run(FORWARD);
   RMotor->run(BACKWARD);
-  Serial.println((char)state);
   Serial.println("Jaquel is turning right");
   }
   else if (state == '4') {
   RMotor->run(BACKWARD);
   LMotor->run(BACKWARD);
-  Serial.println((char)state);  
   Serial.println("Jaquel is going backwards");
   }
   else{
